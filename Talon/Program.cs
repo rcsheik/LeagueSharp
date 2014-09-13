b35391dd26fa8861b77b24cad46973a0bf73a184
madk <isntmeokay@gmail.com>
2014-09-13 20:57:58 +0100

    Update Talon
    Menu fixed
    Drawings
    New ignite logic
    New Farm logic

--- Talon/Program.cs
+++ Talon/Program.cs
@@ -15,8 +15,8 @@ namespace Talon
         private static Menu Menu;
         private static Orbwalking.Orbwalker Orbwalker;
         private static Spell Q, W, E, R;
-        private static Items.Item GB, TMT, HYD, SOTD;
         private static SpellSlot Ignite;
+        private static Items.Item GB, TMT, HYD, SOTD;
 
         private static Obj_AI_Hero LockedTarget;
         
@@ -35,14 +35,12 @@ namespace Talon
 
             Menu = new Menu("Talon", "Talon", true);
 
-
             var SimpleTS = new Menu("Target Selector", "Target Selector");
             SimpleTs.AddToMenu(SimpleTS);
             Menu.AddSubMenu(SimpleTS);
 
             Menu.AddSubMenu(new Menu("Orbwalker", "Orbwalker"));
             Orbwalker = new Orbwalking.Orbwalker(Menu.SubMenu("Orbwalker"));
-
 
             Menu.AddSubMenu(new Menu("Combo", "combo"));
             Menu.SubMenu("combo").AddItem(new MenuItem("combo_Q", "Q").SetValue(true));
@@ -62,10 +60,24 @@ namespace Talon
             Menu.SubMenu("farm").AddItem(new MenuItem("farm_W", "W").SetValue(true));
 
             Menu.AddSubMenu(new Menu("Items", "items"));
-            Menu.SubMenu("items").AddItem(new MenuItem("item_GB", "Ghostblade"));
-            Menu.SubMenu("items").AddItem(new MenuItem("item_TMT", "Tiamat"));
-            Menu.SubMenu("items").AddItem(new MenuItem("item_HYD", "Hydra"));
-            Menu.SubMenu("items").AddItem(new MenuItem("item_SOTD", "SOTD"));
+            Menu.SubMenu("items").AddItem(new MenuItem("item_GB", "Ghostblade").SetValue(true));
+            Menu.SubMenu("items").AddItem(new MenuItem("item_TMT", "Tiamat").SetValue(true));
+            Menu.SubMenu("items").AddItem(new MenuItem("item_HYD", "Hydra").SetValue(true));
+            Menu.SubMenu("items").AddItem(new MenuItem("item_SOTD", "SOTD").SetValue(true));
+
+            Menu.AddSubMenu(new Menu("Drawings", "drawings"));
+            Menu.SubMenu("drawings").AddItem(new MenuItem("draw_W", "W & E").SetValue(new Circle(true, System.Drawing.Color.White)));
+            Menu.SubMenu("drawings").AddItem(new MenuItem("draw_R", "R").SetValue(new Circle(true, System.Drawing.Color.White)));
+            
+            // From Esk0r's Syndra
+            var dmgAfterCombo = Menu.SubMenu("drawings").AddItem(new MenuItem("draw_Dmg", "Draw HP after Combo").SetValue(true));
+
+            Utility.HpBarDamageIndicator.DamageToUnit = GetComboDamage;
+            Utility.HpBarDamageIndicator.Enabled = dmgAfterCombo.GetValue<bool>();
+            dmgAfterCombo.ValueChanged += delegate(object sender, OnValueChangeEventArgs eventArgs)
+            {
+                Utility.HpBarDamageIndicator.Enabled = eventArgs.GetNewValue<bool>();
+            };
 
             Menu.AddToMainMenu();
 
@@ -75,14 +87,13 @@ namespace Talon
             E = new Spell(SpellSlot.E, 700f);
             R = new Spell(SpellSlot.R, 500f);
 
+            Ignite = Player.GetSpellSlot("summonerdot", true);
 
             // Items
             GB = new Items.Item(3142, 0f);
             TMT = new Items.Item(3077, 400f);
             HYD = new Items.Item(3074, 400f);
             SOTD = new Items.Item(3131, 0f);
-
-            Ignite = Player.GetSpellSlot("summonerdot", true);
 
             Game.OnGameUpdate += Game_OnGameUpdate;
             Drawing.OnDraw += Drawing_OnDraw;
@@ -91,6 +102,7 @@ namespace Talon
 
         private static void Game_OnGameUpdate(EventArgs args)
         {
+            // Reset Locked Target for Ultimate Rush
             if (LockedTarget != null && (LockedTarget.IsDead || Player.IsDead || GetComboDamage(LockedTarget) < LockedTarget.Health))
                 LockedTarget = null;
 
@@ -121,10 +133,14 @@ namespace Talon
 
         private static void Drawing_OnDraw(EventArgs args)
         {
-            foreach(Obj_AI_Hero Enemy in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsEnemy && !hero.IsDead && hero.IsValid && hero.Health < GetComboDamage(hero)))
-            {
-                Utility.DrawCircle(Enemy.Position, 200, System.Drawing.Color.Red);
-            }
+            var drawWE = Menu.Item("draw_W").GetValue<Circle>();
+            var drawR = Menu.Item("draw_R").GetValue<Circle>();
+
+            if (drawWE.Active)
+                Utility.DrawCircle(Player.Position, W.Range, drawWE.Color);
+
+            if (drawR.Active)
+                Utility.DrawCircle(Player.Position, R.Range, drawR.Color);
         }
 
         private static void Perform_Combo()
@@ -132,37 +148,53 @@ namespace Talon
             var useW = Menu.Item("combo_W").GetValue<bool>();
             var useE = Menu.Item("combo_E").GetValue<bool>();
             var useR = Menu.Item("combo_R").GetValue<bool>();
+            var useI = Menu.Item("combo_IGN").GetValue<bool>();
+
+            var useGB = Menu.Item("item_GB").GetValue<bool>();
+            var useTMT = Menu.Item("item_TMT").GetValue<bool>();
+            var useHYD = Menu.Item("item_HYD").GetValue<bool>();
+            var useSOTD = Menu.Item("item_SOTD").GetValue<bool>();
+
             var useRush = Menu.Item("combo_RUSH").GetValue<bool>();
 
             var Target = LockedTarget != null ? LockedTarget : SimpleTs.GetTarget(1500f, SimpleTs.DamageType.Physical);
 
+            // Ultimate Rush
             if(UltimateRush(Target) && useRush)
             {
                 LockedTarget = Target;
                 R.Cast();
             }
 
-            if (TMT.IsReady() && Target.IsValidTarget(TMT.Range))
+            // Items
+            if (TMT.IsReady() && Target.IsValidTarget(TMT.Range) && useTMT)
                 TMT.Cast();
 
-            if (HYD.IsReady() && Target.IsValidTarget(HYD.Range))
+            if (HYD.IsReady() && Target.IsValidTarget(HYD.Range) && useHYD)
                 HYD.Cast();
 
-            if (GB.IsReady() && Target.IsValidTarget(Orbwalking.GetRealAutoAttackRange(Player)))
+            if (GB.IsReady() && Target.IsValidTarget(Orbwalking.GetRealAutoAttackRange(Player)) && useGB)
                 GB.Cast();
 
-            if(SOTD.IsReady() && Target.IsValidTarget(Orbwalking.GetRealAutoAttackRange(Player)))
+            if(SOTD.IsReady() && Target.IsValidTarget(Orbwalking.GetRealAutoAttackRange(Player)) && useSOTD)
                 SOTD.Cast();
 
+            // Spells
             if (E.IsReady() && Target.IsValidTarget(E.Range) && useE)
                 E.CastOnUnit(Target);
             else if (W.IsReady() && Target.IsValidTarget(W.Range) && useW)
                 W.Cast(Target.Position);
             else if (R.IsReady() && Target.IsValidTarget(R.Range) && useR && DamageLib.getDmg(Target, DamageLib.SpellType.R) > Target.Health)
                 R.Cast();
-            else if (Ignite != SpellSlot.Unknown && Player.Spellbook.GetSpell(Ignite).State == SpellState.Ready && DamageLib.getDmg(Target, DamageLib.SpellType.IGNITE) > Target.Health && Target.IsValidTarget(600f))
-                Player.Spellbook.CastSpell(Ignite, Target);
-
+            
+            // Auto Ignite
+            if(useI && Ignite != SpellSlot.Unknown && Player.SummonerSpellbook.CanUseSpell(Ignite) == SpellState.Ready)
+                foreach(var enemy in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsEnemy && hero.IsValidTarget(600f) && !hero.IsDead && hero.Health < DamageLib.getDmg(hero, DamageLib.SpellType.IGNITE)).OrderByDescending(hero => SimpleTs.GetPriority(hero)))
+                {
+                    Player.SummonerSpellbook.CastSpell(Ignite, enemy);
+                    return;
+                }
+                
         }
 
         private static void Perform_Harass()
@@ -177,13 +209,26 @@ namespace Talon
         }
 
         private static void Perform_Clear()
-        {            
-            Obj_AI_Minion Minion = ObjectManager.Get<Obj_AI_Minion>().Where(minion => minion.Team != Player.Team && !minion.IsDead && minion.Distance(Player) < 700f ).OrderBy(minion => minion.Distance(Player)).ToArray()[0];
-            
-            var useW = Menu.Item("farm_W").GetValue<bool>();
+        {
+            if (!Menu.Item("farm_W").GetValue<bool>()) return;
 
-            if (useW && Minion != null)
-                W.Cast(Minion.Position);   
+            // Logic from HellSing's ViktorSharp
+            List<Obj_AI_Base> Minions = MinionManager.GetMinions(Player.Position, W.Range, MinionTypes.All, MinionTeam.NotAlly);
+            int hitCount = 0;
+            Obj_AI_Base target = null;
+            foreach(var Minion in Minions)
+            {
+                var hits = MinionManager.GetBestLineFarmLocation((from mnion in MinionManager.GetMinions(Minion.Position, W.Range - Player.Distance(Minion.Position), MinionTypes.All, MinionTeam.NotAlly) select mnion.Position.To2D()).ToList<Vector2>(), 300f, W.Range).MinionsHit;
+
+                if (hitCount < hits)
+                {
+                    hitCount = hits;
+                    target = Minion;
+                }
+            }
+
+            if (target != null)
+                W.Cast(target.Position);   
         }
 
         private static bool UltimateRush(Obj_AI_Hero target)
@@ -196,44 +241,52 @@ namespace Talon
             return true;
         }
 
-        private static double GetComboDamage(Obj_AI_Hero target)
+        private static float GetComboDamage(Obj_AI_Base target)
         {
             double Damage = 0;
 
+            var useQ = Menu.Item("combo_Q").GetValue<bool>();
+            var useW = Menu.Item("combo_W").GetValue<bool>();
+            var useE = Menu.Item("combo_E").GetValue<bool>();
+            var useR = Menu.Item("combo_R").GetValue<bool>();
+            var useRUSH = Menu.Item("combo_RUSH").GetValue<bool>();
+            var useTMT = Menu.Item("item_TMT").GetValue<bool>();
+            var useHYD = Menu.Item("item_HYD").GetValue<bool>();
+            var useSOTD = Menu.Item("item_SOTD").GetValue<bool>();
+
             // Q
-            if(Q.IsReady())
+            if(Q.IsReady() && useQ)
                 Damage += DamageLib.getDmg(target, DamageLib.SpellType.Q, DamageLib.StageType.FirstDamage);
 
             // W
-            if(W.IsReady())
+            if(W.IsReady() && useW)
                 Damage += DamageLib.getDmg(target, DamageLib.SpellType.W);
 
             // R
-            if(E.IsReady())
+            if(E.IsReady() && (useR || useRUSH))
                 Damage += DamageLib.getDmg(target, DamageLib.SpellType.R);
 
-
             // Double AA + SOTD
-            int SOTDbonus = SOTD.IsReady() ? 1 : 2;
+            int SOTDbonus = SOTD.IsReady() && useSOTD ? 1 : 2;
             Damage += ((DamageLib.getDmg(target, DamageLib.SpellType.AD) * 1.1 * (Q.IsReady() ? 2 : 1)) * SOTDbonus);
 
 
             //  Tiamat
-            if (TMT.IsReady())
+            if (TMT.IsReady() && useTMT)
                 Damage += DamageLib.getDmg(target, DamageLib.SpellType.TIAMAT);
 
 
             // Hydra
-            if (HYD.IsReady())
+            if (HYD.IsReady() && useHYD)
                 Damage += DamageLib.getDmg(target, DamageLib.SpellType.HYDRA);
 
             // E damage amplification
             double[] Amp = { 0, 1.03, 1.06, 1.09, 1.12, 1.15 };
 
-            if(E.IsReady())
+            if(E.IsReady() && useE)
                 Damage += Damage * Amp[E.Level];
 
-            return Damage;
+            return (float) Damage;
         }
 
     }
